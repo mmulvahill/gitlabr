cc72ad1883343fdc0f2c4f11d23d40506cd36b6e
Matt (MBP15) <matthew.mulvahill@charter.com>
2019-06-22 15:47:50 -0600

    updated debug script w/ instructions and clearer code

--- debug.R
+++ debug.R
@@ -1,19 +1,61 @@
+################################################################################
+# Testing the Gitlab API package
+#
+# Notes:
+#   - API documentation https://docs.gitlab.com/ee/api/
+#   - Save gitlab credentials here: ~/.gitlab.credentials
+#      - your credentials are just a one line string PAT created here: 
+#        https://gitlab.spectrumxg.com/profile/personal_access_tokens
+################################################################################
 
-library(devtools)
+
+# Packages
+library(remotes)
 library(magrittr)
+library(tidyverse)
+# library(git2r)
 
-document()
-check()
-install("./")
 
+# Functions used in debugging & rebuilding pkgs
+# library(devtools)
+# document()
+# check()
+# install("./")
+
+
+
+# Load gitlab api package
+install_github("mmulvahill/gitlabr", ref = "fix_duplicate_colnames_error")
 library(gitlabr)
 
+
+# Data Science group has a group ID number of:
+data_science_grp <- 1712
+
+# Create connection
 my_gitlab <- gl_connection("https://gitlab.spectrumxg.com", private_token = readLines("~/.gitlab.credentials"))
-# my_gitlab("projects", pages = 2)
-mine_issues <- my_gitlab(c("groups", 1712, "issues"))
 
-mine_issues %>% 
-  mutate_at()
+# Grab all issues for a group (first vector arg specifies API path - https://docs.gitlab.com/ee/api/)
+ds_issues <- my_gitlab(c("groups", data_science_grp, "issues"))
 
+# mine_issues %>% 
+#   mutate_at()
 
+ds_members <- my_gitlab(c("groups", data_science_grp, "members"))
+
+ds_members %>% print(n = 100)
+
+ds_members %>% filter(!(username %in% c("gsureddi", "marjorie.truitt", "Ilchenko", "SFields")))
+
+# events <- my_gitlab(c("groups", data_science_grp, "events"))
+
+# Data science boards
+boards <- my_gitlab(c("groups", data_science_grp, "boards"))
   
+boards %>% dplyr::filter(., name == "Credential Sharing") %>% dplyr::select(dplyr::contains("lists.label.nam"))
+
+
+boards %>% dplyr::filter(., name == "Credential Sharing") %>% dplyr::select(dplyr::contains("lists.label.nam"))
+
+
+
